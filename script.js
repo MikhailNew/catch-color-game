214148146b121cbe741924e4928b3f8aab429fb5
MikhailNew <56970417+MikhailNew@users.noreply.github.com>
2020-06-04 16:03:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+var $start = document.querySelector('#start');
+var $game = document.querySelector('#game');
+var $time = document.querySelector('#time');
+var $timeHeader = document.querySelector('#time-header');
+var $resultHeader= document.querySelector('#result-header');
+var $result = document.querySelector('#result');
+var $gameTime = document.querySelector('#game-time');
+
+/*Счет в игре, количество нажатий на квадрат*/
+var score = 0;
+/*Условия работы игры*/
+var isGameStarted = false;
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+$gameTime.addEventListener('input', setGameTime);
+
+/*Создадим функции, чтобы неповторятся (DRY)*/
+function show($show){
+	$show.classList.remove('hide');
+}
+function hide($hide){
+	$hide.classList.add('hide');
+}
+
+
+//Начало игры
+function startGame() {
+	setGameTime();
+	score = 0;
+	$gameTime.setAttribute('disabled', 'true');
+	isGameStarted = true;
+	hide($start);
+	$game.style.backgroundColor = '#fff';
+	
+	/*Создаем интервал для расчета времени*/
+	var interval = setInterval(function() {
+		var time = parseFloat($time.textContent);
+		if (time <= 0){
+			clearInterval(interval);
+			endGame();
+		} else {
+			$time.textContent = (time - 0.1).toFixed(1);
+		}
+	}, 100);
+	renderBox();
+}
+
+function setGameTime(){
+	var time = +$gameTime.value;
+	$time.textContent = time.toFixed(1);
+	show($timeHeader);
+	hide($resultHeader);
+}
+
+/*Выводим результат в конце игры*/
+function setGameScore(){
+	$result.textContent = score.toString()
+}
+
+/*Создаем функцию для прекращения игры, когда время 0*/
+function endGame(){
+	isGameStarted = false;
+	setGameScore();
+	show($start);
+	$game.style.backgroundColor = '#ccc';
+	$game.innerHTML= '';
+	hide($timeHeader);
+	show($resultHeader);
+	$gameTime.removeAttribute('disabled');
+}
+
+/*Создаем отзывчивость по клику в поле game на 
+динамические эелементы*/
+function handleBoxClick(event){
+
+	if (!isGameStarted){
+		return 
+		/*для того, что бы функция ничего не выводила
+		оставляем пустой ретурн*/
+	}
+
+	/*Если у эелемента есть дата-бокс, 
+	то значит, что мы кликнули именно по квадрату
+	внутри игрового поля, у которого нет дата-бокс*/
+	if (event.target.dataset.box){
+		score++;
+		renderBox();
+	}
+}
+
+/*Создаем функцию, которая будет генерировать 
+квадраты*/
+function renderBox(){
+
+	$game.innerHTML = '';
+	var box = document.createElement('div');
+	var boxSize = getRandom(30,100);
+	var gameSize = $game.getBoundingClientRect();
+
+	/*Максимальное отклонение от верхней части поля*/
+	var maxTop = gameSize.height - boxSize;
+	var maxLeft = gameSize.width - boxSize;
+
+	box.style.height = box.style.width = boxSize+'px';
+	box.style.position = 'absolute';
+	box.style.backgroundColor = getRandomColor();
+	box.style.top = getRandom(0, maxTop) + 'px';
+	box.style.left = getRandom(0,maxLeft) + 'px';
+	box.style.cursor = 'pointer';
+	box.setAttribute('data-box', 'true');
+
+	$game.insertAdjacentElement('afterbegin', box);
+
+}
+
+/*Создаем функцию, генерирующую рандомные квадраты*/
+function getRandom(min,max){
+	return Math.floor(Math.random() *(max-min)+min);
+}
+
+function getRandomColor(){
+	var r=Math.floor(Math.random() * (256));
+    var g=Math.floor(Math.random() * (256));
+    var b=Math.floor(Math.random() * (256));
+    return 'rgb'+'('+r+', '+g+', '+b+', '+'.'+getRandom(5,150)+')';
+}
